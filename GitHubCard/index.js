59c962d0fd52b42c794b27e1cad1f20399e10ea9
Funmi7 <talabifunmilayo@gmail.com>
2019-09-06 00:21:57 +0100

    Made the code more readable and shorter

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,21 +3,47 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get('https://api.github.com/users/funmi7')
-  .then( response => {
-     cardContainer.appendChild(githubCreator(response.data));
-  })
- 
-  .catch(error => {
-    console.log('error: ', error);
-  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
-   Skip to Step 3.
+/* Step 3: Create a function that accepts a single object as its only argument,
+          Using DOM methods and properties, create a component that will return the following DOM element:
+<div class="card">
+  <img src={image url of user} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div>
 */
+/* Step 4: Pass the data received from Github into your function, 
+          create a new component and add it to the DOM as a child of .cards
+*/
+  
+//Using axios to send a GET request
+axios.get('https://api.github.com/users/funmi7')
+.then( response => {
+//Passing the data received into my function
+const githubCreatorData = githubCreator(response.data);
+   cardContainer.appendChild(githubCreatorData);
+})
+
+.catch(error => {
+  console.log('error: ', error);
+});
+
+
 const cardContainer = document.querySelector('.cards');
+
+//A component function
 function githubCreator({'avatar_url':avatarUrl, name, login, location, 'html_url':htmlUrl, followers, following, bio}) {
   const cardDiv = document.createElement('div');
   const image = document.createElement('img');
@@ -48,21 +74,16 @@ function githubCreator({'avatar_url':avatarUrl, name, login, location, 'html_url
 
   cardDiv.appendChild(image);
   cardDiv.appendChild(cardInfoDiv);
-  cardInfoDiv.appendChild(usersName);
-  cardInfoDiv.appendChild(userName);
-  cardInfoDiv.appendChild(locations);
-  cardInfoDiv.appendChild(profile);
-  profile.appendChild(profileUrl);
-  cardInfoDiv.appendChild(userFollowers);
-  cardInfoDiv.appendChild(userFollowing);
-  cardInfoDiv.appendChild(bios);
+  
+  let elements = [usersName, userName, locations, profile, userFollowers,
+                   userFollowing, bios]
 
+  elements.map((element => {
+    cardInfoDiv.appendChild(element);
+  }))
+  profile.appendChild(profileUrl);
   return cardDiv;
 }
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -75,42 +96,20 @@ function githubCreator({'avatar_url':avatarUrl, name, login, location, 'html_url
 */
 
 const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
 function gitLinks(gitHandles) {
- return  axios.get(`https://api.github.com/users/${gitHandles}`);
-}
-followersArray.forEach((item) => {
-  gitLinks(item)
-  .then( response => {
-    cardContainer.appendChild(githubCreator(response.data))
-    })
-  .catch(error => {
-    console.log(error);
-  })
-})
-
-
-
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-*/
-
-
+  return  axios.get(`https://api.github.com/users/${gitHandles}`);
+ }
+ followersArray.forEach((item) => {
+   gitLinks(item)
+   .then( response => {
+    githubCreatorData = githubCreator(response.data);
+     cardContainer.appendChild(githubCreatorData);
+     })
+   .catch(error => {
+     console.log(error);
+   })
+ })
 
 /* List of LS Instructors Github username's: 
   tetondan
